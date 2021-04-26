e74796e28fb79d62dd881155dfa5fa1b5a10a9bf
Dylan <dylanhoffman651@gmail.com>
2021-04-25 19:51:54 -0700

    Apple Picker Dev
    The Picking of Apples

---
+++ Apple Picker Prototype/Assets/Scripts/ApplePicker.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ApplePicker : MonoBehaviour
+{
+    public GameObject basketPrefab;
+    public int numBaskets = 3;
+    public float basketBottomY = -14f;
+    public float basketSpacingY = 2f;
+    public List<GameObject> basketList;
+
+    void Start()
+    {
+        basketList = new List<GameObject>();
+        for (int i=0; i<numBaskets; i++)
+        {
+            GameObject tBasketGo = Instantiate<GameObject>(basketPrefab);
+            Vector3 pos = Vector3.zero;
+            pos.y = basketBottomY + (basketSpacingY * i);
+            tBasketGo.transform.position = pos;
+            basketList.Add(tBasketGO);
+        }
+    }
+
+    public void AppleDestroyed()
+    {
+        GameObject[] tAppleArray = GameObject.FindGameObjectsWithTag("Apple");
+        foreach (GameObject tGO in tAppleArray)
+        {
+            Destroy(tGO);
+            if (basketList.Count == 0)
+            {
+                SceneManager.LoadScene("_Scene_0");
+            }
+        }
+        int basketIndex = basketList.Count - 1;
+        GameObject tBasketGO = basketList[basketIndex];
+        basketList.RemoveAt(basketIndex);
+        Destroy(tBasketGO);
+    }
+}
